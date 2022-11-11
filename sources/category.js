a1d17b751423782d964a270e21ac534ab0300a0d
Abhinav068 <abhinav28897@gmail.com>
2022-11-11 23:20:32 +0530

    product page addded

--- sources/category.js
+++ sources/category.js
@@ -1,184 +1,198 @@
-let People_type=['Men','Women','Boys','Girls'];
-People_type.forEach((el,i)=>{
-    let prnt=document.getElementById('people_type');
-    let person=document.createElement('div');
-    person.addEventListener('click',(el1)=>{
-        Click_unclick_0(el1,i);        
-        // console.log(el.target.parentElement.childNodes[0].checked);        
-    })
-    let input=document.createElement('input');
-    input.type='radio';
-    input.id=`pType${i}`;
-    let pType=document.createElement('span');
-    pType.textContent=el;
-    
-    person.append(input,pType);   
-    prnt.append(person);
+let People_type = ["Men", "Women", "Boys", "Girls"];
+People_type.forEach((el, i) => {
+  let prnt = document.getElementById("people_type");
+  let person = document.createElement("div");
+  person.addEventListener("click", (el1) => {
+    Click_unclick_0(el1, i);
+    // console.log(el.target.parentElement.childNodes[0].checked);
+  });
+  let input = document.createElement("input");
+  input.type = "radio";
+  input.id = `pType${i}`;
+  let pType = document.createElement("span");
+  pType.textContent = el;
+
+  person.append(input, pType);
+  prnt.append(person);
 });
-function Click_unclick_0(el,i){
-    // console.log(el,i);
-    el.target.parentElement.childNodes[0].checked=true;
-    People_type.forEach((el2,ind)=>{
-        if(ind!=i){
-            document.getElementById(`pType${ind}`).checked=false;
-        }      
-    })
+function Click_unclick_0(el, i) {
+  // console.log(el,i);
+  el.target.parentElement.childNodes[0].checked = true;
+  People_type.forEach((el2, ind) => {
+    if (ind != i) {
+      document.getElementById(`pType${ind}`).checked = false;
+    }
+  });
 }
 
-
 let concepts = [
-    { code: "BASICS", count: 10 },
-    { code: "H&M MAN", count: 23 },
-    { code: "H&M Move", count: 1 },
-    { code: "MODERN CLASSIC", count: 50 }
-]
-concepts=concepts.sort((a, b) => b.count - a.count);
-concepts.forEach((el,i)=>{
-    let prnt=document.getElementById('brand');
-    let brd=document.createElement('div');
-    brd.addEventListener('click',(el1)=>{
-        Click_unclick_1(el1,i);        
-        // console.log(el.target.parentElement.childNodes[0].checked);        
-    })
-    let input=document.createElement('input');
-    input.type='radio';
-    input.id=`brand${i}`;
-    let brdName=document.createElement('span');
-    brdName.textContent=el.code;
-    let brdCount=document.createElement('span');
-    brdCount.textContent=` (${el.count})`;
-    brd.append(input,brdName,brdCount);   
-    prnt.append(brd);
-})
-function Click_unclick_1(el,i){
-    // console.log(el,i);
-    el.target.parentElement.childNodes[0].checked=true;
-    concepts.forEach((el2,ind)=>{
-        if(ind!=i){
-            document.getElementById(`brand${ind}`).checked=false;
-        }      
-    })
+  { code: "BASICS", count: 10 },
+  { code: "H&M MAN", count: 23 },
+  { code: "H&M Move", count: 1 },
+  { code: "MODERN CLASSIC", count: 50 },
+];
+concepts = concepts.sort((a, b) => b.count - a.count);
+concepts.forEach((el, i) => {
+  let prnt = document.getElementById("brand");
+  let brd = document.createElement("div");
+  brd.addEventListener("click", (el1) => {
+    Click_unclick_1(el1, i);
+    // console.log(el.target.parentElement.childNodes[0].checked);
+  });
+  let input = document.createElement("input");
+  input.type = "radio";
+  input.id = `brand${i}`;
+  let brdName = document.createElement("span");
+  brdName.textContent = el.code;
+  let brdCount = document.createElement("span");
+  brdCount.textContent = ` (${el.count})`;
+
+  brd.append(input, brdName, brdCount);
+  prnt.append(brd);
+});
+function Click_unclick_1(el, i) {
+  // console.log(el,i);
+  el.target.parentElement.childNodes[0].checked = true;
+  concepts.forEach((el2, ind) => {
+    if (ind != i) {
+      document.getElementById(`brand${ind}`).checked = false;
+    }
+  });
 }
 
 let colorWithNames = [
-    { code: 'red', count: 28 },
-    { code: 'yellow', count: 38 },
-    { code: 'green', count: 18 },
-    { code: 'pink', count: 15 },
-    { code: 'blue', count: 20 },
-    { code: 'green', count: 38 },
-    { code: 'white', count: 8 },
-    { code: 'purple', count: 23 },
-    { code: 'green', count: 18 },
-    { code: 'turquoise', count: 25 },
-    { code: 'orange', count: 10 },
-]
-colorWithNames=colorWithNames.sort((a, b) => b.count - a.count)
-colorWithNames.forEach((el,i)=>{
-    let prnt=document.getElementById('colors');
-    let clr=document.createElement('div');
-    clr.addEventListener('click',(el1)=>{
-        Click_unclick_2(el1,i);        
-        // console.log(el.target.parentElement.childNodes[0].checked);        
-    })
-    let input=document.createElement('input');
-    input.type='radio';
-    input.id=`color${i}`;
-    let clrName=document.createElement('span');
-    clrName.textContent=el.code;
-    let clrCount=document.createElement('span');
-    clrCount.textContent=` (${el.count}) `;
-    clr.append(input,clrName,clrCount);   
-    prnt.append(clr);
-})
+  { code: "red", count: 28 },
+  { code: "yellow", count: 38 },
+  { code: "green", count: 18 },
+  { code: "pink", count: 15 },
+  { code: "blue", count: 20 },
+  { code: "green", count: 38 },
+  { code: "white", count: 8 },
+  { code: "purple", count: 23 },
+  { code: "green", count: 18 },
+  { code: "turquoise", count: 25 },
+  { code: "orange", count: 10 },
+];
+colorWithNames = colorWithNames.sort((a, b) => b.count - a.count);
+colorWithNames.forEach((el, i) => {
+  let prnt = document.getElementById("colors");
+  let clr = document.createElement("div");
+  clr.addEventListener("click", (el1) => {
+    Click_unclick_2(el1, i);
+    // console.log(el.target.parentElement.childNodes[0].checked);
+  });
+  let input = document.createElement("input");
+  input.type = "radio";
+  input.id = `color${i}`;
+  input.style.accentColor = el.code;
+  let clrName = document.createElement("span");
+  clrName.textContent = el.code;
+  let clrCount = document.createElement("span");
+  clrCount.textContent = ` (${el.count}) `;
+  clr.append(input, clrName, clrCount);
+  prnt.append(clr);
+});
 
-function Click_unclick_2(el,i){
-    // console.log(el,i);
-    el.target.parentElement.childNodes[0].checked=true;
-    colorWithNames.forEach((el2,ind)=>{
-        if(ind!=i){
-            document.getElementById(`color${ind}`).checked=false;
-        }      
-    })
-    
+function Click_unclick_2(el, i) {
+  // console.log(el,i);
+
+  el.target.parentElement.childNodes[0].checked = true;
+  colorWithNames.forEach((el2, ind) => {
+    if (ind != i) {
+      document.getElementById(`color${ind}`).checked = false;
+    }
+  });
 }
 
+const fetching = async (value) => {
+  let res = await fetch(
+    `https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?categories=${value}&pagesize=5&currentpage=1&`,
+    {
+      params: {
+        country: "in",
+        lang: "en",
+      },
+      headers: {
+        'X-RapidAPI-Key': 'ac6ca5b643msh3d3bf01d93278adp1458dfjsn66f2b23af94c',
+        'X-RapidAPI-Host': 'apidojo-hm-hennes-mauritz-v1.p.rapidapi.com'
+      }
+    }
+  );
 
-let Pdata=[
-    {image:'https://rukminim1.flixcart.com/image/832/832/xif0q/shirt/r/a/y/4xl-fo-shrt-pekok-5th-anfold-original-imafkzvqg4qptzg4-bb.jpeg?q=70',
-    title:'formal shirt',
-    MRP: 200,
-    discount: 10,
-    actual_price:150   
-},
-{image:'https://rukminim1.flixcart.com/image/832/832/xif0q/shirt/r/a/y/4xl-fo-shrt-pekok-5th-anfold-original-imafkzvqg4qptzg4-bb.jpeg?q=70',
-    title:'formal shirt',
-    MRP: 200,
-    discount: 10,
-    actual_price:150   
-},
-{image:'https://rukminim1.flixcart.com/image/832/832/xif0q/shirt/r/a/y/4xl-fo-shrt-pekok-5th-anfold-original-imafkzvqg4qptzg4-bb.jpeg?q=70',
-    title:'formal shirt',
-    MRP: 200,
-    discount: 10,
-    actual_price:150   
-},
-{image:'https://rukminim1.flixcart.com/image/832/832/xif0q/shirt/r/a/y/4xl-fo-shrt-pekok-5th-anfold-original-imafkzvqg4qptzg4-bb.jpeg?q=70',
-    title:'formal shirt',
-    MRP: 200,
-    discount: 10,
-    actual_price:150   
-},
-{image:'https://rukminim1.flixcart.com/image/832/832/xif0q/shirt/r/a/y/4xl-fo-shrt-pekok-5th-anfold-original-imafkzvqg4qptzg4-bb.jpeg?q=70',
-    title:'formal shirt',
-    MRP: 200,
-    discount: 10,
-    actual_price:150   
-},
-{image:'https://rukminim1.flixcart.com/image/832/832/xif0q/shirt/r/a/y/4xl-fo-shrt-pekok-5th-anfold-original-imafkzvqg4qptzg4-bb.jpeg?q=70',
-    title:'formal shirt',
-    MRP: 200,
-    discount: 10,
-    actual_price:150   
-}
-]
+  let data = await res.json();
+  console.log(data.results);
+  localStorage.setItem("tempData", JSON.stringify(data.results));
+  append(data.results);
+};
+// fetching("men_party");
 
-Pdata.forEach((el,i)=>{
-    
-    let parent=document.getElementById('data');  
-    
-    let card=document.createElement('div');
-    card.className='card';
-    card.addEventListener('click',()=>{
-        console.log(`working${i}`);
-    })
-    let image=document.createElement('img');
-    image.src=el.image;
-    
-    let title=document.createElement('h3');
-    title.textContent=el.title;
+let tempo_data = JSON.parse(localStorage.getItem("tempData")) || [];
+append(tempo_data);
+function append(Rdata) {
+  //   let Pdata = [
+  //     {
+  //       image: Rdata[0].images[0].url,
 
-    let prices=document.createElement('div');
-    prices.className='prices';
-    let MRP=document.createElement('p');
-    MRP.textContent='Rs.'+el.MRP+' ';
-    MRP.style.textDecoration='line-through';
-    let discount=document.createElement('p');
-    discount.textContent=el.discount+'% OFF';
+  //       title: Rdata[0].name,
+  //       MRP: Math.floor(Rdata[0].price.value * 80.97),
+  //       discount: 10,
+  //       actual_price: 150,
+  //     },
+  //     {
+  //       image: Rdata[1].images[0].url,
+
+  //       title: Rdata[1].name,
+  //       MRP: Math.floor(Rdata[1].price.value * 80.97),
+  //       discount: 10,
+  //       actual_price: 150,
+  //     },
+  //     {
+  //       image: Rdata[2].images[0].url,
+
+  //       title: Rdata[2].name,
+  //       MRP: Math.floor(Rdata[2].price.value * 80.97),
+  //       discount: 10,
+  //       actual_price: 1503
+  //     },
+  //   ];
+
+  Rdata.forEach((el, i) => {
+    let discnt = Math.floor(Math.random() * 10) + 10;
+    let parent = document.getElementById("data");
+
+    let card = document.createElement("div");
+    card.className = "card";
+    card.addEventListener("click", () => {
+      console.log(`working${i}`);
+      localStorage.setItem("pro_key", JSON.stringify(el.articleCodes[0]));
+      localStorage.setItem("pro_images", JSON.stringify(el.galleryImages));
+      let prices = [Math.floor(mrp - mrp / discnt), mrp, discnt];
+      localStorage.setItem("prices", JSON.stringify(prices));
+
+      window.location.href = "product.html";
+    });
+    let image = document.createElement("img");
+    image.src = el.images[0].url;
+
+    let title = document.createElement("h3");
+    title.textContent = el.name;
+
+    let prices = document.createElement("div");
+    prices.className = "prices";
+    let MRP = document.createElement("p");
+    let mrp = Math.floor(el.price.value * 80.97);
+    MRP.textContent = "Rs." + mrp + " ";
+    MRP.style.textDecoration = "line-through";
+    let discount = document.createElement("p");
+    discount.textContent = discnt + "% OFF";
     discount.style.color = "red";
-    let actual_price=document.createElement('h4');
-    actual_price.textContent='Rs:'+(el.MRP-(el.MRP/el.discount))+' ';
-    prices.append(actual_price,MRP,discount);
+    let actual_price = document.createElement("h4");
+    actual_price.textContent = "Rs:" + Math.floor(mrp - mrp / discnt) + "  ";
+    prices.append(actual_price, MRP, discount);
 
-    card.append(image,title,prices);
-    parent.append(card);   
+    card.append(image, title, prices);
+    parent.append(card);
+  });
+}
 
-})
-
-
-
-
-
-
-
- // actual_price: this.MRP-((this.MRP/this.discount)*100)
+// actual_price: this.MRP-((this.MRP/this.discount)*100)
--- sources/product.js
+++ sources/product.js
@@ -1,3 +1,41 @@
+let key = JSON.parse(localStorage.getItem("pro_key"));
+let p_images = JSON.parse(localStorage.getItem("pro_images"));
+
+console.log(key, p_images);
+const detail = async (value) => {
+  let res = await fetch(
+    `https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/detail?productcode=${value}`,
+    {
+      params: {
+        country: "in",
+        lang: "en",
+      },
+      headers: {
+        'X-RapidAPI-Key': 'ac6ca5b643msh3d3bf01d93278adp1458dfjsn66f2b23af94c',
+        'X-RapidAPI-Host': 'apidojo-hm-hennes-mauritz-v1.p.rapidapi.com'
+      }
+    }
+  );
+
+  let data = await res.json();
+  console.log(data.product);
+  Append(data.product);
+};
+detail(key);
+
+// left side
+p_images.forEach((el, i) => {
+  if (i < 6) {
+    let img_parent = document.getElementById("images");
+    let img_div = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = el.url;
+    img_div.append(img);
+    img_parent.append(img_div);
+  }
+});
+
+//right side
 product = {
   brand: "abc",
   name: "xyz",
@@ -30,27 +68,42 @@ product = {
   ],
 };
 
-let parent = document.getElementById("p_name");
-let p_brand = document.createElement("div");
-p_brand.textContent = product.brand;
-let p_name = document.createElement("div");
-p_name.textContent = product.name;
-let p_rating=document.createElement("div");
-p_rating.textContent=product.rating+'⭐| '+product.rating_count;
+function Append(info) {
+  let pro_prices=JSON.parse(localStorage.getItem('prices'));
+  let parent = document.getElementById("p_detail");
+  let p_name = document.createElement("h3");
+  p_name.textContent = info.name;
 
-let hr_line=document.createElement('hr');
+  let p_price = document.createElement("div");
+  p_price.id='prices'
+  let p_prc = document.createElement("h3");
+  p_prc.textContent = `Rs. ${
+   pro_prices[0]
+  } `;
+  let p_MRP = document.createElement("h3");
+  p_MRP.className = "MRP";
+  p_MRP.textContent = `Rs. ${pro_prices[1]} `;
+  p_MRP.style.textDecoration='line-through';
+  let p_discount = document.createElement("h3");
+  p_discount.textContent = `(${pro_prices[2]}%)`;
+  p_price.append(p_prc, p_MRP, p_discount); 
 
-let p_price = document.createElement("div");
-let p_prc=document.createElement('span');
-p_prc.textContent=`Rs. ${product.MRP-(product.MRP*product.discount/100)} `;
-let p_MRP=document.createElement('span');
-p_MRP.className='MRP';
-p_MRP.textContent=`Rs. ${product.MRP} `;
-let p_discount=document.createElement('span');
-p_discount.textContent=`${product.discount}%`;
-p_price.append(p_prc,p_MRP,p_discount);
+  let color=document.createElement('h4');
+  color.textContent=info.color.text;
+  let pImage_div=document.createElement('div');
+  pImage_div.id='small_images';
+  let img1_div=document.createElement('div');
+  let img1=document.createElement('img');
+  img1.src=p_images[0].url;
+  img1_div.append(img1);
+  
+  let img2_div=document.createElement('div');
+  let img2=document.createElement('img');
+  img2.src=p_images[1].url;
+  img2_div.append(img2);
 
-let text=document.createElement('h6');
-text.textContent=`inclusive all taxes`;
-parent.append(p_brand,p_name,p_rating,hr_line,p_price,text);
+  pImage_div.append(img1_div,img2_div);
+  parent.append(p_name,p_price,color,pImage_div);
+}
 
+// sizes

19bcd412e579de32e69b8d77737cccf543e6df25
pratap562 <kumpratap562@gmail.com>
2022-11-12 17:09:38 +0530

    admin and orders database wishlist and cart

--- my code/scripts/admin_signin.js
+++ my code/scripts/admin_signin.js
@@ -1,25 +1,25 @@
 
 
-class User{
+class User {
 
-    constructor(){
+    constructor() {
 
     }
 
-    async Login(u,p){
+    async Login(u, p) {
         const login_data = {
-            username:u,
-            password:p,
+            username: u,
+            password: p,
         };
 
         const login_api = 'https://masai-api-mocker.herokuapp.com/auth/login'
 
-        const response = await fetch(login_api,{
-            method:'POST',
-            body:JSON.stringify(login_data),
+        const response = await fetch(login_api, {
+            method: 'POST',
+            body: JSON.stringify(login_data),
 
             headers: {
-                'Content-Type':'application/json',
+                'Content-Type': 'application/json',
             },
         });
         const data = await response.json();
@@ -30,16 +30,16 @@ class User{
 
 let user = new User()
 
-const Login = async () =>{
+const Login = async () => {
     const username = document.getElementById('email').value
     const password = document.getElementById("password").value
 
-    let { token } = await user.Login(username,password);
+    let { token } = await user.Login(username, password);
     // console.log(token)
 
-    profile(username,token)
-    
-    if(token !== null){
+    profile(username, token)
+
+    if (token !== null) {
         window.location.href = "admin.html";
     }
 }
@@ -50,20 +50,20 @@ loginBtn.onclick = () => {
     Login()
 }
 
-const profile = async (username,token) => {
+const profile = async (username, token) => {
     let api_link = `https://masai-api-mocker.herokuapp.com/user/${username}`
 
-    let response = await fetch(api_link,{
-        headers:{
-            Authorization : `Bearer ${token}`,
+    let response = await fetch(api_link, {
+        headers: {
+            Authorization: `Bearer ${token}`,
             'Content-Type': 'application/json'
         },
     });
     let data = await response.json();
     // console.log(data)
-    
+
     let user_name = data.username
-    localStorage.setItem("name",JSON.stringify(user_name))
+    localStorage.setItem("name", JSON.stringify(user_name))
 }
 
 //End of day-2
@@ -71,3 +71,16 @@ const profile = async (username,token) => {
 
 //start of day-3
 
+
+// orders = {
+//     date, customerName, customerId, productId, productName, price, paymentStatus, productUrl
+// }
+
+
+// wishList = {
+//     proeductName, price, imgUrl, customerId
+// }
+
+// cart = {
+//     proeductName, price, imgUrl, customerId
+// }
